8a72f741daa22f841f3814100bd274fec4438ba8
Jinghong18 <3128819644@qq.com>
2025-03-28 23:11:36 +0800

    first commit

---
+++ src/pages/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <RouterView />
+    <van-tabbar v-model="active">
+        <van-tabbar-item v-for="item in router.options.routes[0].children" :key="item.path" :icon="item.meta.icon" :url="`#/${item.path}`">{{item.meta.name}}</van-tabbar-item>
+    </van-tabbar>
+
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+
+const active = ref(0)
+
+onMounted(() => {
+    const data =router.options.routes[0]
+    active.value = data.children.findIndex(item => '/' + item.path === route.path)
+})
+</script>
